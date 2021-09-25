92846bb6da4f233e743d1acb1246e257f493b556
unknown <hassan.hk.ka@gmail.com>
2021-09-25 12:34:05 +0330

    users templates added

--- users/models.py
+++ users/models.py
@@ -9,13 +9,25 @@ class Profile(models.Model):
     email = models.EmailField(max_length=500, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     profile_img = models.ImageField(blank=True, null=True, upload_to='profiles/', default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, null=True, blank=True)
     social_twitter = models.CharField(max_length=200, null=True, blank=True)
     social_linkedin = models.CharField(max_length=200, null=True, blank=True)
-    social_youtube = models.CharField(max_length=200, null=True, blank=True)
+    social_stackoverflow = models.CharField(max_length=200, null=True, blank=True)
+    social_website = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.username)
+class Skill(models.Model):
+
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
